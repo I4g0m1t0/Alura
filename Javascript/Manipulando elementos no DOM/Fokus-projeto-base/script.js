874a8ebc8372f6b07a3153d55af4d740d6262959
Iago <iagorechtramontin@gmail.com>
2024-12-04 16:48:22 -0300

    Aula 04

--- Javascript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
+++ Javascript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
@@ -6,8 +6,14 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-1.mp3');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 musica.loop = true;
 
 musicaFocoInput.addEventListener('change', () => {
@@ -57,3 +63,27 @@ function alterarContexto (contexto) {
             break;
     }
 };
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        zerar();
+        alert('Tempo finalizado');
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+}
+
+startPauseBt.addEventListener('click', iniciar);
+
+function iniciarOuPAusar() {
+    if(intervaloId) {
+        zerar();
+        return;
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
