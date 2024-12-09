d1edd3d5fa5f343376cdabb9bfa3676a9e5707c9
Iago <iagorechtramontin@gmail.com>
2024-12-09 17:06:51 -0300

    Fim da aula -4

--- Javascript/Explorando a manipulação de elementos e da localStorage/fokus-local-storage-main/script-crud.js
+++ Javascript/Explorando a manipulação de elementos e da localStorage/fokus-local-storage-main/script-crud.js
@@ -4,10 +4,12 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
-
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []; //Parse é o inverso o stringify
+let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -49,6 +51,24 @@ function criarElementoTarefa(tarefa) {
     li.append(paragrafo);
     li.append(botao);
 
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active');
+            });
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = '';
+            tarefaSelecionada = null;
+            liTarefaSelecionada = null;
+            return;
+        }
+        tarefaSelecionada = tarefa;
+        liTarefaSelecionada = li;
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+        
+        li.classList.add('app__section-task-list-item-active');
+    }
+
     return li;
 }
 
@@ -81,4 +101,13 @@ const limparFormulario = () => {
     textarea.value = '';
     formAdicionarTarefa.classList.add('hidden');
 }
+
 btnCancelar.addEventListener('click', limparFormulario);
+
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+    }
+})
