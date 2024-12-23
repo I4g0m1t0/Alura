badb4308da67c628da77c64e2d1f09acd2f6ef1c
Iago <iagorechtramontin@gmail.com>
2024-12-23 17:05:27 -0300

    Fim da aula 03

--- Javascript/JavaScript métodos de array/alura_books-projeto_inicial/app/metodoForEach.js
+++ Javascript/JavaScript métodos de array/alura_books-projeto_inicial/app/metodoForEach.js
@@ -1,19 +1,21 @@
-const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+const elementoParaInserirLivros = document.getElementById('livros')
 
-function exibirLivrosNaTela(listaDeLivros) {
+function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="livro__imagens" src="${livro.imagem}"
+          alt="${livro.alt}" />
         <h2 class="livro__titulo">
-            ${livro.titulo}
+          ${livro.titulo}
         </h2>
         <p class="livro__descricao">${livro.autor}</p>
         <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
         <div class="tags">
-            <span class="tag">${livro.categoria}</span>
+          <span class="tag">${livro.categoria}</span>
         </div>
-        </div>
+      </div>
         `
-    });
+    })
 }
