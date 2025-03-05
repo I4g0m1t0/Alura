e00562a27e1aaf5ba33f79dc39ba87a53ad6b3f2
Iago <iagorechtramontin@gmail.com>
2025-03-05 15:29:45 -0300

    Fim da aula 02

---
+++ Javascript/JavaScript programando a Orientação a Objetos/index.js
@@ -0,0 +1,44 @@
+class Cliente {
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    // _saldo = 0;
+    #saldo = 0;
+
+    sacar(valor){
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor <= 0) {
+            return;
+        }
+        this.#saldo += valor;
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(valorSacado);
+
+console.log(cliente1);
+console.log(cliente2);
