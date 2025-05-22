93153ba860060aa1a65720c73b536283fd377ef6
Iago <iagorechtramontin@gmail.com>
2025-05-21 21:21:54 -0300

    Fim da Aula 02. Herança e reaproveitamento de código

---
+++ Typescript/typescript-curso-2/dist/js/views/view.js
@@ -0,0 +1,11 @@
+export class View {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
+    update(model) {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
+//Abstract para forçar que seja implementado os métodos
+//<T> é um Generics
